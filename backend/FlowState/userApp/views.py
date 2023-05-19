97a020e1c2e73fe4b2c16f2760ee21769e43b70b
Prabhakar Mishra <prabhakarmishra222@gmail.com>
2023-05-19 23:11:40 +0530

    Added dynamic components

--- backend/FlowState/userApp/views.py
+++ backend/FlowState/userApp/views.py
@@ -15,7 +15,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'explore.html')
+    return render(request,'index.html')
 
 def mylogin(request):
     if request.method == "POST":
